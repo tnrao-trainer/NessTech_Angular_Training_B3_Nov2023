81b19db64f05d956ce5994fd33b4f8014054f6ee
tnrao-trainer <40280456+tnrao-trainer@users.noreply.github.com>
2023-11-16 18:02:34 +0530

    Add files via upload

---
+++ 01.Day1/Examples/Eg1_CustomPipe_Gender/gender.pipe.ts
@@ -0,0 +1,17 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'gender'
+})
+export class GenderPipe implements PipeTransform {
+
+    transform(input:string):  string{      
+      let output:string  = "";
+
+      if(input == "m" || input == "M")  output="Male";
+      if(input == "f"  ||  input == "F")  output="Female";
+
+      return output;
+    }
+
+}
